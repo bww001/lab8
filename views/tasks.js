862c39fea4ba6b6cd605359678b25af5fdd54c27
Briana <bww001@ucsd.edu>
2017-03-02 11:36:58 -0800

    partials

---
+++ views/tasks.js
@@ -0,0 +1,5 @@
+<form id='taskFor'' action='/addTask' method='post'>
+<label for='addTask'>Add a task</label>
+<input id='addTask' name='taskName' placeholder='New Task' />
+<button type='submit' class='btn btn-primary'>Submit</button>
+</form>
